306af6c9617e43baf708cfa69887129a20bdadb1
robert_nad <TaxWithholding>
2019-03-05 17:13:46 +0200

    append new functions, improve presto_ssh client

--- presto_ssh.sh
+++ presto_ssh.sh
@@ -1,5 +1,17 @@
-#!/usr/bin/env bash
-source logging.sh
+#!/bin/bash
+
+source ~/env.sh
+source ${COMMON_SCRIPTS}/logging.sh
+
+if [[ -z ${SSH_HOST_PRESTO} ]]; then
+    error "SSH_PORT_PRESTO and SSH_HOST_PRESTO are empty"
+    exit ${NOT_PROVIDED_CONN_SETTINGS}
+else
+    info "port ${SSH_PORT_PRESTO} and host ${SSH_HOST_PRESTO}"
+fi
+
+
+
 
 
 NOT_PROVIDED_CONN_SETTINGS=120
@@ -9,7 +21,7 @@ NOT_EXISTS_QUERY_FILE=2
 verbosity=5
 function remote_execute(){
    debug "ssh ${SSH_HOST_PRESTO} $1"
-   ssh ${SSH_HOST_PRESTO} -f $1
+   ssh -P ${SSH_PORT_PRESTO} ${SSH_HOST_PRESTO} $1
 }
 
 
@@ -17,38 +29,28 @@ function ssh_presto_client() {
     #todo append uuid
     local query_path=$1
     local result_path=$2
-    local file_name=$(basename ${query_path})
+    local query_file_name=$(basename ${query_path} | cut -f1 -d.)
+    local result_file_name=$(date '+%Y_%d_%m_%H_%M_%S').csv
     if ! [[ -z result_path ]]; then
-        result_path=$(dirname $(dirname ${query_path}))/result/${file_name}.csv
+        mkdir -p $(dirname $(dirname ${query_path}))/result/${query_file_name}
+        result_path=$(dirname $(dirname ${query_path}))/result/${query_file_name}/${result_file_name}
     fi
-    local remote_path=/tmp/${file_name}
-    local remote_out_path=/tmp/${file_name}.csv
+    local remote_query_path=/tmp/${query_file_name}
+    local remote_out_path=/tmp/${result_file_name}
     info "check connection to remote host"
     remote_execute "ls" 2>&1 > /dev/null && ! [[ $? -eq 0 ]] &&  error "connection was failed" && exit ${CONN_FAILED}
     info "load queries file to remote machine"
-    scp ${query_path} ${SSH_HOST_PRESTO}:${remote_path}
+    scp ${query_path} ${SSH_HOST_PRESTO}:${remote_query_path}
     info "exists of remote queries"
-    remote_execute "test -e ${remote_path}"
+    remote_execute "test -e ${remote_query_path}"
     ! [[ $? -eq 0 ]] && error "not exitst remote queries" &&  exit ${NOT_EXISTS_QUERY_FILE}
     info "execute of script"
-    remote_execute "presto --execute \"$(cat ${query_path})\" --output-format CSV_HEADER > ${remote_out_path}"
+
+    remote_execute "presto -f ${remote_query_path} --schema ${PRESTO_SILVER} --output-format CSV_HEADER > ${remote_out_path}"
     remote_execute "test -e ${remote_out_path}"
     ! [[ $? -eq 0 ]] && error "not exitst remote queries" &&  exit ${NOT_EXISTS_QUERY_FILE}
     scp ${SSH_HOST_PRESTO}:${remote_out_path} ${result_path}
-    return ${running}
+#    return ${running}
 }
 
-
-if [[ -z ${SSH_HOST_PRESTO} ]]; then
-    echo "try to load env"
-    source ~/env.sh
-    if [[ -z ${SSH_HOST_PRESTO} ]]; then
-        error "SSH_PORT_PRESTO and SSH_HOST_PRESTO are empty"
-        exit ${NOT_PROVIDED_CONN_SETTINGS}
-    else
-        info "port ${SSH_PORT_PRESTO} and host ${SSH_HOST_PRESTO}"
-    fi
-
-fi
-
-ssh_presto_client $1
+ssh_presto_client $1 $2
--- utils/alias.sh
+++ utils/alias.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+function mem {
+  vm_stat | perl -ne '/page size of (\d+)/ and $size=$1; /Pages\s+([^:]+)[^\d]+(\d+)/ and printf("%-16s % 16.2f Mi\n", "$1:", $2 * $size / 1048576);'
+}
 
 echo "alias loaded"
-
+alias smem="mem"
 alias gh="history | grep"
-alias 'tmux --kill-all'="tmux ls | grep : | cut -d. -f1 | awk '{print substr(\$1, 0, length(\$1)-1)}' | (while IFS='' read -r p1; do if ! [ -z \${p1} ]; then tmux kill-session -t \$p1; else echo \$p1; fi; done)"
+#alias 'tmux --kill-all'="tmux ls | grep : | cut -d. -f1 | awk '{print substr(\$1, 0, length(\$1)-1)}' | (while IFS='' read -r p1; do if ! [ -z \${p1} ]; then tmux kill-session -t \$p1; else echo \$p1; fi; done)"
 alias gps="ps aux -xl | grep"
