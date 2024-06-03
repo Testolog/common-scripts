dbbdeed3f2642d2918743b9a595305811b080c22
testolog <testolog5@gmail.com>
2024-06-03 13:05:52 +0300

    append new tools for processing of history commands

---
+++ utils/history_helper.sh
@@ -0,0 +1,14 @@
+function exists { which $1 &> /dev/null }
+function percol_select_history() {
+    local tac
+    exists gtac && tac="gtac" || { exists tac && tac="tac" || { tac="tail -r" } }
+    BUFFER=$(fc -l -n 1 | eval $tac | percol --query "$LBUFFER")
+    CURSOR=$#BUFFER         # move cursor
+    zle -R -c               # refresh
+}
+if exists percol; then
+    zle -N percol_select_history
+    bindkey '^R' percol_select_history
+else
+  pip install percol
+fi
