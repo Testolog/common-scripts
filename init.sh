807c68a27f0b2b765ae4d86f0d197bf4ff02bfde
testolog <testolog5@gmail.com>
2024-06-03 13:05:53 +0300

    append nvim configuration

--- init.sh
+++ init.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-function atom_plugins() {
-  apm install tablr autocomplete-sql autosave-onchange script
-  return ${PIPESTATUS[0]}
-}
-
-if [[ -x "$(command -v apm)" && ! -e ${AMP_INIT} ]]; then
-  echo "install atom_plugins"
-  atom_plugins
-  touch ${AMP_INIT}
-  #todo: append check of error
-fi
+#todo
---
+++ nvim/lua/general.lua
@@ -0,0 +1,8 @@
+local keymap = require("settings")
+local plugins = require("plugins")
+local colorschema="catppuccin"
+return {
+ keymap=keymap;
+ plugins=plugins;
+ colorschema=colorschema;
+}
